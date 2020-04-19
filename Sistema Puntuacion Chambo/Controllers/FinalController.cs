96e364b2004211322d44dc6a24f12a2c395b82ef
CRISTIAN GERMAN PERALTA YUMI <cgperalta.fis@unach.edu.ec>
2020-04-19 17:57:25 -0500

    Modificacion de Informe final

--- Sistema Puntuacion Chambo/Controllers/FinalController.cs
+++ Sistema Puntuacion Chambo/Controllers/FinalController.cs
@@ -42,39 +42,27 @@ namespace Sistema_Puntuacion_Chambo.Controllers
         }
         public ActionResult Informe()
         {
+            List<byte> copied = new List<byte>();
+            List<byte> result = new List<byte>();
 
-            //var ViewAsString = RenderViewAsString("Index", tbl_candidatas);
-            //htmlToPdf.PageWidth = 1600;
-            //htmlToPdf.PageHeight = 900;
-            //string ViewContent = 
-            var htmlContent = String.Format("<body>Hello world: {0}" +
-                "<h1>Rayios</h1></body>", DateTime.Now);
+            ViewBag.CandidatasList = db.tbl_candidatas;
+            ViewBag.JuecesList = db.tbl_users_jueces.Where(x => x.tipo == "U").ToList();
+            // Agregar rango a List && Conversion de ObjetoResult a List 
+            ViewBag.Notario = db.tbl_users_jueces.Where(x => x.tipo == "N").ToList();
+            foreach (var z in db.tbl_candidatas)
+            {
+                var m = db.PromediarNotas(z.id);
+                // Agregar rango a List && Conversion de ObjetoResult a List 
+                copied.AddRange(m.Where(x => x.HasValue).Select(x => x.Value));
+            }
+            ViewBag.participantes = copied;
+            return View(db.tbl_users_jueces.Where(x => x.tipo == "U").Count());
 
-
-            var htmlToPdf = new NReco.PdfGenerator.HtmlToPdfConverter();
-            //var pdfBytes = htmlToPdf.GeneratePdfFromFile("~/Views/Final/Index", null);
-            var pdfBytes = htmlToPdf.GeneratePdf(htmlContent);
-            FileResult FileResult = new FileContentResult(pdfBytes, "application/pdf");
-
-            return FileResult;
-
-            //DateTime.Now);
         }
 
-    
-    public ActionResult Final()
-    {
-            if (Session["username"] == null)
-            {
-                return RedirectToAction("Login", "Uno");
-
-            }
-            else
-                return View();
-    }
-        public ActionResult Print()
+         public ActionResult Print()
         {
-            return new ActionAsPdf("Index") { FileName = "ResultadoFinal.pdf" };
+            return new ActionAsPdf("Informe") { FileName = "ResultadoFinal.pdf" };
 
         }
     }
